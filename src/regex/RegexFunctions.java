691e24b9729ac91dcdea57744439e3a00010a14d
Xinyu Wang <xwang@cs.utexas.edu>
2018-09-09 16:26:47 -0500

    Updated regex grammar and lexicon

--- src/regex/RegexFunctions.java
+++ src/regex/RegexFunctions.java
@@ -4,9 +4,19 @@ import edu.stanford.nlp.sempre.NameValue;
 
 public class RegexFunctions {
 
-  public static NameValue contain(NameValue regex) {
+  public static NameValue contain(NameValue arg0) {
 
-    String id = "contain(" + regex.id + ")";
+    String id = "contain(" + arg0.id + ")";
+
+    NameValue ret = new NameValue(id);
+
+    return ret;
+
+  };
+
+  public static NameValue and(NameValue arg0, NameValue arg1) {
+
+    String id = "and(" + arg0.id + "," + arg1.id + ")";
 
     NameValue ret = new NameValue(id);
 
