8d0adb6c37943050c27f941370b3aa0572362677
Xinyu Wang <xwang@cs.utexas.edu>
2018-09-09 09:38:16 -0500

    Added a regex package in src, added build for regex package, added some initial grammars and a test script

---
+++ test.sh
@@ -0,0 +1,3 @@
+#./run @mode=simple -Grammar.inPaths regex_grammars/sql.grammar -SimpleLexicon.inPaths regex_grammars/sql.lexicon
+./run @mode=simple -Grammar.inPaths regex_grammars/regex.grammar -SimpleLexicon.inPaths regex_grammars/regex.lexicon
+#./run @mode=simple -Grammar.inPaths data/tutorial-arithmetic.grammar
