0d5638a282ec615a63a7c82aff7d53bf23ba865c
thelyad <sc.lycoris.bv@gmail.com>
2018-09-12 13:56:53 -0500

    update grammar and lexicon

--- src/regex/RegexFunctions.java
+++ src/regex/RegexFunctions.java
@@ -24,4 +24,57 @@ public class RegexFunctions {
 
   };
 
+  public static NameValue startwith(NameValue arg0) {
+
+    String id = "startwith(" + arg0.id + ")";
+
+    NameValue ret = new NameValue(id);
+
+    return ret;
+
+  };
+
+  public static NameValue endwith(NameValue arg0) {
+
+    String id = "endwith(" + arg0.id + ")";
+
+    NameValue ret = new NameValue(id);
+
+    return ret;
+
+  };
+
+  public static NameValue repeat(NameValue arg0, Integer arg1) {
+    
+    String id = "repeat(" + arg0.id + "," + arg1 + ")";
+    
+    NameValue ret = new NameValue(id);
+    
+    return ret;
+  };
+
+  public static NameValue followedby(NameValue arg0, NameValue arg1) {
+    String id = "followedby(" + arg0.id + "," + arg1.id + ")";
+    
+    NameValue ret = new NameValue(id);
+    
+    return ret;
+  };
+
+  public static NameValue not(NameValue arg0) {
+    String id = "not(" + arg0.id + ")";
+    
+    NameValue ret = new NameValue(id);
+    
+    return ret;
+  };
+
+  public static NameValue or(NameValue arg0, NameValue arg1) {
+    String id = "or(" + arg0.id + "," + arg1.id + ")";
+    
+    NameValue ret = new NameValue(id);
+    
+    return ret;
+  }
+
 }
