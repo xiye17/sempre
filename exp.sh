204355e3c62540f1e793b7c6c474352a5b09adcc
thelyad <sc.lycoris.bv@gmail.com>
2018-09-12 17:18:16 -0500

    update main and exp script

--- exp.sh
+++ exp.sh
@@ -4,20 +4,20 @@ dataset=0
 # beam = 200, 1k data set
 beam=200
 dataset="regex/data/turk_1k/regex.examples"
-java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Main -Grammar.inPaths regex/regex.grammar -SimpleLexicon.inPaths regex/regex.lexicon -Parser.beamSize $beam  -FeatureExtractor.featureDomains rule -Dataset.inPaths train:$dataset -Learner.maxTrainIters 3 > b200ds1k
+java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Main -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths regex/regex.grammar -SimpleLexicon.inPaths regex/regex.lexicon -Parser.beamSize $beam  -FeatureExtractor.featureDomains rule -Dataset.inPaths train:$dataset -Learner.maxTrainIters 3 > b200ds1k
 
 
 # beam = 1k, 1k data set
 beam=1000
 dataset="regex/data/turk_1k/regex.examples"
-java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Main -Grammar.inPaths regex/regex.grammar -SimpleLexicon.inPaths regex/regex.lexicon -Parser.beamSize $beam  -FeatureExtractor.featureDomains rule -Dataset.inPaths train:$dataset -Learner.maxTrainIters 3 > b1kds1k
+java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Main -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths regex/regex.grammar -SimpleLexicon.inPaths regex/regex.lexicon -Parser.beamSize $beam  -FeatureExtractor.featureDomains rule -Dataset.inPaths train:$dataset -Learner.maxTrainIters 3 > b1kds1k
 
 
 
 # beam = 200, 10k data set
 beam=200
 dataset="regex/data/turk/regex.examples"
-java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Main -Grammar.inPaths regex/regex.grammar -SimpleLexicon.inPaths regex/regex.lexicon -Parser.beamSize $beam  -FeatureExtractor.featureDomains rule -Dataset.inPaths train:$dataset -Learner.maxTrainIters 3 > b200ds10k
+java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Main -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths regex/regex.grammar -SimpleLexicon.inPaths regex/regex.lexicon -Parser.beamSize $beam  -FeatureExtractor.featureDomains rule -Dataset.inPaths train:$dataset -Learner.maxTrainIters 3 > b200ds10k
 
 
 
@@ -25,6 +25,6 @@ java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Main -Grammar.inPaths reg
 
 beam=1000
 dataset="regex/data/turk/regex.examples"
-java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Main -Grammar.inPaths regex/regex.grammar -SimpleLexicon.inPaths regex/regex.lexicon -Parser.beamSize $beam  -FeatureExtractor.featureDomains rule -Dataset.inPaths train:$dataset -Learner.maxTrainIters 3 > b1kds10k
+java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Main -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths regex/regex.grammar -SimpleLexicon.inPaths regex/regex.lexicon -Parser.beamSize $beam  -FeatureExtractor.featureDomains rule -Dataset.inPaths train:$dataset -Learner.maxTrainIters 3 > b1kds10k
 
 
--- src/regex/Main.java
+++ src/regex/Main.java
@@ -2,25 +2,44 @@ package regex;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import edu.stanford.nlp.sempre.Builder;
 import edu.stanford.nlp.sempre.Dataset;
 import edu.stanford.nlp.sempre.Derivation;
 import edu.stanford.nlp.sempre.Learner;
 import edu.stanford.nlp.sempre.Master;
+import edu.stanford.nlp.sempre.Parser;
 import edu.stanford.nlp.sempre.Session;
 import fig.basic.LogInfo;
 import fig.exec.Execution;
 
 public class Main implements Runnable {
 
-  public String filePath = "regex/data/test/src-train.txt";
+//  public String filePath = "regex/data/test/src-train.txt";
+//  public String src_filePath = "regex/data/test/src-t-2.txt";
+  public String src_filePath = "";
+//  public String filePath = "regex/data/test/src-c.txt";
+//  public String gt_filePath = "regex/data/test/spec-train.txt";
+//  public String spec_filePath = "regex/data/test/spec-t-2.txt";
+  public String spec_filePath = "";
+//  public String ig_filePath = "regex/data/test/ignore.txt";
+//  public String noCoverage_filePath = "regex/data/test/noCoverage.txt";
+  public String noCoverage_filePath = "";
+//  public String stat_filePath = "regex/data/test/stats.txt";
+  public String stat_filePath = "";
+  
+  int currIndx = 1;
 
   @Override
   public void run() {
-
+   
     Builder builder = new Builder();
     builder.build();
 
@@ -33,52 +52,149 @@ public class Main implements Runnable {
     Master master = new Master(builder);
 
     Session session = master.getSession("stdin");
-
-    try (BufferedReader br = new BufferedReader(new FileReader(new File(filePath)))) {
-
-      for (String line = br.readLine(); line != null;) {
-
-        int indent = LogInfo.getIndLevel();
-        try {
-
-          Master.Response response = master.processQuery(session, line);
-
-          // get derivations from response 
-          // UNCOMMENT this to see how it prints 
-
-//          List<Derivation> derivations = response.ex.getPredDerivations();
-//
-//          for (Derivation derivation : derivations) {
-
+    
+    // findout file path
+    String dPath = Dataset.opts.inPaths.get(0).getSecond();
+    if(dPath.equals("regex/data/turk_1k/regex.examples")) {
+      src_filePath = "regex/data/turk_1k/src-test.txt";
+      spec_filePath = "regex/data/turk_1k/spec-test.txt";
+      
+      noCoverage_filePath = "b" + Parser.opts.beamSize + "ds1k_noCoverage";
+      stat_filePath = "b" + Parser.opts.beamSize + "ds1k_stat";
+      
+    }
+    
+    else if(dPath.equals("regex/data/turk/regex.examples")) {
+      src_filePath = "regex/data/turk/src-test.txt";
+      spec_filePath = "regex/data/turk/spec-test.txt";
+      
+      noCoverage_filePath = "b" + Parser.opts.beamSize + "ds10k_noCoverage";
+      stat_filePath = "b" + Parser.opts.beamSize + "ds10k_stat";
+    }
+    // for testing 
+    else if(dPath.equals("regex/data/t/regex.examples")) {
+      src_filePath = "regex/data/t/src-test.txt";
+      spec_filePath = "regex/data/t/spec-test.txt";
+      
+      noCoverage_filePath = "b" + Parser.opts.beamSize + "ds1k_noCoverage";
+      stat_filePath = "b" + Parser.opts.beamSize + "ds1k_stat";
+      
+    }
+    
+    int coverageCount = 0;
+    int accurateCount = 0;
+    
+    List<Integer> noCoverageIndx = new ArrayList<Integer>();
+    
+    // stat: print the coverage, accuracy percentage and unique set count
+    try(PrintWriter stat = new PrintWriter(stat_filePath)) {
+            
+      // noCoverage: print the benchmarks that are not able to covered      
+      try(PrintWriter noCoverage = new PrintWriter(new FileOutputStream(new File(noCoverage_filePath), true))) {
+        
+        // brGt: read ground truth
+        try(BufferedReader brGt = new BufferedReader(new FileReader(new File(spec_filePath)))) {
           
-          // This prints: 
-          // (name "contain(fb:en.let)") --> we want contain(fb:en.let) [extract the substring from first space to last "] 
-          // (name fb:en.let) --> we want fb:en.let 
-          //            System.out.println(derivation.value);
+          // br: read source
+          try (BufferedReader br = new BufferedReader(new FileReader(new File(src_filePath)))) {
 
-//            System.out.println(derivation);
-//          }
-//
-//          assert false;
+            for (String line = br.readLine(); line != null;) {
+              
+              // read ground truth 
+              String gtLine = brGt.readLine();
 
-        } catch (Throwable t) {
+              int indent = LogInfo.getIndLevel();
+              
+              try {
 
-          while (LogInfo.getIndLevel() > indent)
-            LogInfo.end_track();
-          t.printStackTrace();
+                Master.Response response = master.processQuery(session, line);
 
+                List<Derivation> derivations = response.ex.getPredDerivations();
+                
+                Set<String> derivStringSet = new HashSet<String>();
+                
+                String topPrediction = "";
+                
+                for(int i = 0; i < derivations.size(); i++) {
+                  String derivString = ((derivations.get(i)).value).toString();
+                  String subDeriv = "";
+                  if(derivString.contains("\"")) {
+                    subDeriv = derivString.split("\"")[1];
+                  } else {
+                    subDeriv = derivString.split(" ")[1];
+                    subDeriv = subDeriv.substring(0, subDeriv.indexOf(")"));
+                  }
+                  derivStringSet.add(subDeriv);
+                  
+                  if(i == 0) {
+                    topPrediction = subDeriv;
+                  }
+                  
+                  System.out.println(subDeriv);
+                }
+                
+                // read the ground truth for the current example
+                System.out.println("gtline:" + gtLine);
+                System.out.println("toppredict: " + topPrediction);
+                
+                // get the unique deriv set size and print to stat
+                stat.println(currIndx + ": " + "unique deriv size = " + derivStringSet.size());
+                
+                // findout if the ground truth is contain in any of the derivation
+                if(derivStringSet.contains(gtLine)) {
+                  coverageCount++;
+                } else {
+                  noCoverageIndx.add(currIndx);
+                  // output format: <benchmark indx>,<ground truth spec>,<top 1 deriviation>
+                  noCoverage.println(currIndx + "," + gtLine + "," + topPrediction);
+                }
+                
+                // findout if the ground truth is the one predicited at the top
+                if(topPrediction.equals(gtLine)) {
+                  accurateCount ++;
+                }
+             
+              } catch (Throwable t) {
+
+                while (LogInfo.getIndLevel() > indent)
+                  LogInfo.end_track();
+                t.printStackTrace();
+
+              }
+
+              line = br.readLine();
+
+              if (line == null || line.isEmpty()) break;
+              
+              currIndx++;
+
+            }
+
+          } catch (Exception exception) {
+
+            exception.printStackTrace();
+
+          }
+        }   
+        catch(Exception e) {
+          e.printStackTrace();
         }
-
-        line = br.readLine();
-
-        if (line == null || line.isEmpty()) break;
-
       }
-
-    } catch (Exception exception) {
-
-      exception.printStackTrace();
-
+      catch(Exception e) {
+        e.printStackTrace();
+      }
+      
+      // output statistics
+      stat.println("----------------------");
+      stat.println("Statistics:");
+      stat.println("----------------------");
+      stat.println("coverage#: " + coverageCount);
+      stat.println("accurate prediction#: " + accurateCount);
+      stat.println("total #: " + currIndx);
+      
+    }
+    catch(Exception e) {
+      e.printStackTrace();
     }
 
   }
