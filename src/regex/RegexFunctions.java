4a06a5bd6d3cf4342f1397b4ab9ea2cb2ff69fae
thelyad <sc.lycoris.bv@gmail.com>
2018-09-26 00:46:00 -0500

    update grammar/lexicon on so benchmark, modify test files

--- src/regex/RegexFunctions.java
+++ src/regex/RegexFunctions.java
@@ -52,6 +52,15 @@ public class RegexFunctions {
 
     return ret;
   };
+  
+  public static NameValue repeat(Integer arg0, Integer arg1, NameValue arg2) {
+
+    String id = "repeat(" + arg2.id + "," + arg0 + "," + arg1 + ")";
+
+    NameValue ret = new NameValue(id);
+
+    return ret;
+  };
 
   public static NameValue followedby(NameValue arg0, NameValue arg1) {
     String id = "followedby(" + arg0.id + "," + arg1.id + ")";
@@ -75,6 +84,38 @@ public class RegexFunctions {
     NameValue ret = new NameValue(id);
 
     return ret;
-  }
+  };
+  
+  public static NameValue optional(NameValue arg0) {
+    String id = "optional(" + arg0.id + ")";
+    
+    NameValue ret = new NameValue(id);
+    
+    return ret;
+  };
+  
+  public static NameValue more(Integer arg0, NameValue arg1) {
+    String id = "repeat(" + arg1.id + "," + arg0 + ",)";
+    
+    NameValue ret = new NameValue(id);
+    
+    return ret;
+  };
+  
+  public static NameValue only(NameValue arg0) {
+    return more(1, arg0);
+  };
+  
+  public static NameValue only(Integer arg0, NameValue arg1) {
+    return repeat(arg1, arg0);
+  };
+  
+  public static NameValue upto(Integer arg0, NameValue arg1) {
+    String id = "repeat(" + arg1.id + ",1," + arg0 + ")";
+    
+    NameValue ret = new NameValue(id);
+    
+    return ret;
+  };
 
 }
--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ dataset=$2
 modeldir="models/b_"$beam"_ds_"$dataset
 params=$modeldir/params
 
-java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Test -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths regex/regex1.grammar -SimpleLexicon.inPaths regex/regex1.lexicon -Parser.beamSize $beam -Builder.inParamsPath $params $dataset
+#java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Test -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths regex/regex1.grammar -SimpleLexicon.inPaths regex/regex1.lexicon -Parser.beamSize $beam -Builder.inParamsPath $params $dataset
+java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.Test -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths regex/regex1.grammar -SimpleLexicon.inPaths regex/regex1.lexicon -Parser.beamSize $beam $dataset
 
 
