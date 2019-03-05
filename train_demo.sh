3c6664f4ea9e61cba86da8588c528c2556c52613
Xi <xiye17@gmail.com>
2019-03-05 06:45:03 -0600

    update training script

--- train_demo.sh
+++ train_demo.sh
@@ -6,6 +6,5 @@ modeldir="models/b_"$beam"_ds_"$dataset
 
 rm -rf $modeldir
 
-java -Xmx32g -cp libregex/*:libsempre/*:lib/* -ea regex.Train -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths demo/demo.fine.grammar -SimpleLexicon.inPaths demo/demo.lexicon -Parser.beamSize $beam -Parser.dumpAllFeatures true -FeatureExtractor.featureDomains rule span context bigram phrase-category  -Dataset.inPaths train:regex/data/$dataset/regex.examples -Learner.maxTrainIters 5  $modeldir
-
+java -Xmx32g -cp libregex/*:libsempre/*:lib/* -ea regex.Train -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths demo/demo.fn.fine.grammar -SimpleLexicon.inPaths demo/demo.lexicon -Parser.beamSize $beam -Parser.dumpAllFeatures true -FeatureExtractor.featureDomains rule span context bigram phrase-category  -Dataset.inPaths train:regex/data/$dataset/regex.examples -Learner.maxTrainIters 5  $modeldir
 
