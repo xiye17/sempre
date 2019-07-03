96b2093e6670ee6ad758518642c0f6c65a1c2a59
Xi <xiye17@gmail.com>
2019-07-02 23:13:11 -0500

    change demo sh

--- test_demo.sh
+++ test_demo.sh
@@ -5,5 +5,5 @@ dataset=$2
 modeldir="models/b_"$beam"_ds_"$dataset
 params=$modeldir/params
 
-java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.TestDemo -languageAnalyzer corenlp.CoreNLPAnalyzer -parser SketchParser -Grammar.inPaths demo/demo.fn.fine.grammar -SimpleLexicon.inPaths demo/demo.lexicon -FeatureExtractor.featureDomains rule span context bigram phrase-category -Builder.inParamsPath $params -Parser.beamSize $beam $dataset
+java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.TestDemo -languageAnalyzer corenlp.CoreNLPAnalyzer -parser SketchParser -Grammar.inPaths demo/popl.grammar -SimpleLexicon.inPaths demo/popl.lexicon -FeatureExtractor.featureDomains rule span context bigram phrase-category -Builder.inParamsPath $params -Parser.beamSize $beam $dataset
 # java -Xmx2g -cp libregex/*:libsempre/*:lib/* -ea regex.TestDemo -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths demo/demo.lambda.grammar -SimpleLexicon.inPaths demo/demo.lexicon -FeatureExtractor.featureDomains rule span context bigram phrase-category -Builder.inParamsPath $params -Parser.beamSize $beam $dataset
