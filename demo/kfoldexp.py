fc644cde5c5ee65e15d7ae52351d9db5012823de
Xi <xiye17@gmail.com>
2019-07-05 14:12:22 -0500

    script for single retest

--- demo/kfoldexp.py
+++ demo/kfoldexp.py
@@ -21,6 +21,8 @@ def _parse_args():
     parser.add_argument("--nfold", type=int, dest="nfold", default=5)
     parser.add_argument("--beam", type=int, dest="beam", default=500)
     parser.add_argument("--max_iter", type=int, dest="max_iter", default=5)
+    parser.add_argument("--target", type=int, dest="target", default=-1)
+
     
     args = parser.parse_args()
     return args
@@ -106,6 +108,64 @@ def exp(args):
             for deriv in pred["derivations"]:
                 f.write(deriv + "\n")
 
+def single(args):
+    exs = read_ready_file(args.outfile)
+    for ex in exs:
+        ex["formula"] = get_exs_formula(ex)
+    target = args.target
+    num_exs = len(exs)
+    perms = np.random.permutation(num_exs)
+    num_left = num_exs
+    print(perms)
+    print("Target", target)
+    for fold_id in range(args.nfold):
+        fold_prefix = os.path.join("./regex/data", args.dataset + "s" + str(fold_id))
+
+        num_this_fold = int(round(num_left * 1.0 / (args.nfold - fold_id)))
+        print(num_this_fold)
+        start_idx = num_exs - num_left
+        num_left = num_left - num_this_fold
+        end_idx = start_idx + num_this_fold
+        if fold_id == args.nfold - 1:
+            end_idx = num_exs
+        test_idx = perms[start_idx:end_idx].tolist()
+        print(test_idx)
+        
+        if target not in test_idx:
+            continue
+        
+        lines = []
+        for i in test_idx:
+            lines.append(exs[i]["id"] + "\t" + exs[i]["nl"] + "\n")
+        with open(join(fold_prefix, "src-test.txt"), "w") as f:
+            f.writelines(lines)
+        
+        dataset_id = args.dataset + "s" + str(fold_id)
+
+        cmd = "sh ./demo/exp_test.sh {0} {1} {2} {3}".format(args.beam, dataset_id, "popl.grammar", args.max_iter)
+        print(cmd)
+        os.system(cmd)
+    
+    preds = []
+    for fold_id in range(args.nfold):
+        fold_prefix = os.path.join("./regex/data", args.dataset + "s" + str(fold_id))
+        fold_pred_file = join(fold_prefix, str(args.beam)+"-pred-test.txt")
+        fold_preds = read_pred_file(fold_pred_file)
+        print(len(fold_preds))
+        preds.extend(fold_preds)
+
+    preds.sort(key=lambda x: int(x["id"]))
+    print(len(preds))
+    # merge results
+    prefix = join("./regex/data/", args.dataset)
+    pred_file = join(prefix, str(args.beam)+"-pred-test.txt")
+    with open(pred_file, "w") as f:
+        for pred in preds:
+            f.write(pred["id"] + "\t" +str(len(pred["derivations"])) + "\n")
+            for deriv in pred["derivations"]:
+                f.write(deriv + "\n")
+
+
 
 def main():
     args = _parse_args()
@@ -118,6 +178,8 @@ def main():
     if args.action == "exp":
         exp(args)
 
+    if args.action == "single":
+        single(args)
 
 if __name__ == "__main__":
     main()
