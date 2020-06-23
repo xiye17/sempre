41703d46835384df40afc6f1fc3654eb630178f7
Xi <xiye17@gmail.com>
2020-06-23 17:42:20 -0500

    tune turk grammar

---
+++ train_kb13.sh
@@ -0,0 +1,10 @@
+ant regex
+
+beam=$1
+dataset=$2
+modeldir="models/b_"$beam"_ds_"$dataset
+
+rm -rf $modeldir
+
+java -Xmx32g -cp libregex/*:libsempre/*:lib/* -ea regex.Train -languageAnalyzer corenlp.CoreNLPAnalyzer -Grammar.inPaths demo/kb13.grammar -SimpleLexicon.inPaths demo/kb13.lexicon -Parser.beamSize $beam -Parser.dumpAllFeatures true -FeatureExtractor.featureDomains rule span context bigram phrase-category   -parser SketchParser -Dataset.inPaths train:regex/data/$dataset/regex.train.examples -Learner.maxTrainIters 5  $modeldir
+
